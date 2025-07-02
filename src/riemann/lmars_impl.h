3d43238f771c61f91ee6355a2265f5ceddb853cc
snowy/cli <chengcli@umich.edu>
2025-07-02 09:27:56 -0400

    wip

--- src/riemann/lmars_impl.h
+++ src/riemann/lmars_impl.h
@@ -29,20 +29,25 @@ void DISPATCH_MACRO lmars_impl(T *flx, T *wl, T *wr, T *hl, T *hr, T *gammal,
   auto ivy = IVX + ((ivx - IVX) + 1) % 3;
   auto ivz = IVX + ((ivx - IVX) + 2) % 3;
 
+  T wli[5] = {WL(IDN), WL(ivx), WL(ivy), WL(ivz), WL(IPR)};
+  T wri[5] = {WR(IDN), WR(ivx), WR(ivy), WR(ivz), WR(IPR)};
+
   // Enthalpies
-  HL += 0.5 * (SQR(WL(ivx)) + SQR(WL(ivy)) + SQR(WL(ivz))) + WL(IPR) / WL(IDN);
-  HR += 0.5 * (SQR(WR(ivx)) + SQR(WR(ivy)) + SQR(WR(ivz))) + WR(IPR) / WR(IDN);
+  HL += 0.5 * (SQR(wli[IVX]) + SQR(wli[IVY]) + SQR(wli[IVZ])) +
+        wli[IPR] / wli[IDN];
+  HR += 0.5 * (SQR(wri[IVX]) + SQR(wri[IVY]) + SQR(wri[IVZ])) +
+        wri[IPR] / wri[IDN];
 
   // Average density, average sound speed, pressure, velocity
-  auto rhobar = 0.5 * (WL(IDN) + WR(IDN));
+  auto rhobar = 0.5 * (wli[IDN] + wri[IDN]);
   auto gamma_bar = 0.5 * (GAMMAL + GAMMAR);
-  auto cbar = sqrt(0.5 * gamma_bar * (WL(IPR) + WR(IPR)) / rhobar);
+  auto cbar = sqrt(0.5 * gamma_bar * (wli[IPR] + wri[IPR]) / rhobar);
 
-  auto pbar =
-      0.5 * (WL(IPR) + WR(IPR)) + 0.5 * (rhobar * cbar) * (WL(ivx) - WR(ivx));
+  auto pbar = 0.5 * (wli[IPR] + wri[IPR]) +
+              0.5 * (rhobar * cbar) * (wli[IVX] - wri[IVX]);
 
-  auto ubar =
-      0.5 * (WL(ivx) + WR(ivx)) + 0.5 / (rhobar * cbar) * (WL(IPR) - WR(IPR));
+  auto ubar = 0.5 * (wli[IVX] + wri[IVX]) +
+              0.5 / (rhobar * cbar) * (wli[IPR] - wri[IPR]);
 
   // Compute fluxes depending on the sign of ubar
   T rd = 1.0;
@@ -52,30 +57,30 @@ void DISPATCH_MACRO lmars_impl(T *flx, T *wl, T *wr, T *hl, T *hr, T *gammal,
       rd -= WL(ICY + n);
     }
 
-    FLX(IDN) = ubar * WL(IDN) * rd;
+    FLX(IDN) = ubar * wli[IDN] * rd;
     for (int n = 0; n < ny; n++) {
-      FLX(ICY + n) = ubar * WL(IDN) * WL(ICY + n);
+      FLX(ICY + n) = ubar * wli[IDN] * WL(ICY + n);
     }
 
-    FLX(ivx) = ubar * WL(IDN) * WL(ivx) + pbar;
-    FLX(ivy) = ubar * WL(IDN) * WL(ivy);
-    FLX(ivz) = ubar * WL(IDN) * WL(ivz);
-    FLX(IPR) = ubar * WL(IDN) * HL;
+    FLX(ivx) = ubar * wli[IDN] * wli[IVX] + pbar;
+    FLX(ivy) = ubar * wli[IDN] * wli[IVY];
+    FLX(ivz) = ubar * wli[IDN] * wli[IVZ];
+    FLX(IPR) = ubar * wli[IDN] * HL;
   } else {
     // Right side flux
     for (int n = 0; n < ny; n++) {
       rd -= WR(ICY + n);
     }
 
-    FLX(IDN) = ubar * WR(IDN) * rd;
+    FLX(IDN) = ubar * wri[IDN] * rd;
     for (int n = 0; n < ny; n++) {
-      FLX(ICY + n) = ubar * WR(IDN) * WR(ICY + n);
+      FLX(ICY + n) = ubar * wri[IDN] * WR(ICY + n);
     }
 
-    FLX(ivx) = ubar * WR(IDN) * WR(ivx) + pbar;
-    FLX(ivy) = ubar * WR(IDN) * WR(ivy);
-    FLX(ivz) = ubar * WR(IDN) * WR(ivz);
-    FLX(IPR) = ubar * WR(IDN) * HR;
+    FLX(ivx) = ubar * wri[IDN] * wri[IVX] + pbar;
+    FLX(ivy) = ubar * wri[IDN] * wri[IVY];
+    FLX(ivz) = ubar * wri[IDN] * wri[IVZ];
+    FLX(IPR) = ubar * wri[IDN] * HR;
   }
 }
 
