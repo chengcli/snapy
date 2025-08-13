e46ba006c81901dae8547972b67f7240af88301c
mac/cli <chengcli@umich.edu>
2025-08-13 14:47:43 -0400

    wip

--- examples/shock.cpp
+++ examples/shock.cpp
@@ -10,7 +10,7 @@ using namespace snap;
 int main(int argc, char** argv) {
   auto op = MeshBlockOptions::from_yaml("shock.yaml");
   auto block = MeshBlock(op);
-  
+
   auto device = torch::kCPU;
   if (torch::cuda::is_available()) {
     std::cout << "Running on CUDA" << std::endl;
