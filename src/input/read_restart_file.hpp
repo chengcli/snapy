087b108f64491690b77a546c512067d6d9d83e42
mac/cli <chengcli@umich.edu>
2025-11-10 09:27:15 -0500

    wip

--- src/input/read_restart_file.hpp
+++ src/input/read_restart_file.hpp
@@ -32,4 +32,14 @@ void read_restart_file(MeshBlockImpl *pmb, std::string fileid,
   kintera::load_tensors(in_vars, fname);
 }
 
+void set_hydro_interior(MeshBlockImpl *block, torch::Tensor &hydro_w,
+                        Variables &in_vars) {
+  auto interior = block->part({0, 0, 0});
+  hydro_w.index(interior)[IDN] = in_vars["rho"];
+  hydro_w.index(interior)[IVX] = in_vars["vel1"];
+  hydro_w.index(interior)[IVY] = in_vars["vel2"];
+  hydro_w.index(interior)[IVZ] = in_vars["vel3"];
+  hydro_w.index(interior)[IPR] = in_vars["press"];
+}
+
 }  // namespace snap
