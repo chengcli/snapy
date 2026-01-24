af8f01fb55f0a5adb9731e418e285416fb47fcb8
stormy/cli <chengcli@umich.edu>
2026-01-24 15:19:33 -0500

    wip

--- python/csrc/pymesh.cpp
+++ python/csrc/pymesh.cpp
@@ -150,5 +150,7 @@ void bind_mesh(py::module &m) {
       .def("print_cycle_info", &snap::MeshBlockImpl::print_cycle_info)
       .def("finalize", &snap::MeshBlockImpl::finalize)
       .def("device", &snap::MeshBlockImpl::device)
-      .def("check_redo", &snap::MeshBlockImpl::check_redo);
+      .def("check_redo", &snap::MeshBlockImpl::check_redo)
+      .def("get_outputs",
+           [](snap::MeshBlockImpl &self) { return self.output_types; });
 }
--- python/csrc/pyoutput.cpp
+++ python/csrc/pyoutput.cpp
@@ -41,7 +41,9 @@ void bind_output(py::module &m) {
       .ADD_OPTION(std::string, snap::OutputOptionsImpl, file_type)
       .ADD_OPTION(std::string, snap::OutputOptionsImpl, data_format);
 
-  auto pyOutputType = py::class_<snap::OutputType>(m, "OutputType");
+  auto pyOutputType =
+      py::class_<snap::OutputType, std::shared_ptr<snap::OutputType>>(
+          m, "OutputType");
 
   pyOutputType.def(py::init<>())
       .def(py::init<snap::OutputOptions>())
@@ -50,11 +52,12 @@ void bind_output(py::module &m) {
              return fmt::format("OutputType(file_number = {}; next_time = {})",
                                 a.file_number, a.next_time);
            })
-      .def("increment_file_number",
-           [](snap::OutputType &a) { return ++a.file_number; });
+      .def_readwrite("file_number", &snap::OutputType::file_number)
+      .def_readwrite("next_time", &snap::OutputType::next_time);
 
   auto pyNetcdfOutput =
-      py::class_<snap::NetcdfOutput, snap::OutputType>(m, "NetcdfOutput");
+      py::class_<snap::NetcdfOutput, snap::OutputType,
+                 std::shared_ptr<snap::NetcdfOutput>>(m, "NetcdfOutput");
 
   pyNetcdfOutput.def(py::init<snap::OutputOptions>())
       .def("__repr__",
