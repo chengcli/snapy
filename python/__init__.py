1a4843227a677f6592b1988e1c85d7f6e76d0d6b
zoey.zyhu <zoey.zyhu@gmail.com>
2026-04-18 23:09:57 -0400

    Apply the modernized workflows similar to the changes in pyharp

--- python/__init__.py
+++ python/__init__.py
@@ -1,3 +1,5 @@
+from importlib.metadata import PackageNotFoundError, version
+
 import torch
 import pydisort
 import pyharp
@@ -9,4 +11,7 @@ torch.set_default_dtype(torch.float64)
 torch.set_num_threads(1)
 torch.set_num_interop_threads(1)
 
-__version__ = "2.2.0"
+try:
+    __version__ = version("snapy")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
