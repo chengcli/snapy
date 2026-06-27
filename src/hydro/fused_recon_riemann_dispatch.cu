ddfc930e717c1208f3691e3426473a5cbdbea46a
Cheng Li <chengcli@umich.edu>
2026-06-26 23:12:21 -0400

    Compile fused dispatch only with NVSHMEM
    Move the NVSHMEM feature boundary for fused_recon_riemann_dispatch.cu into CMake. Non-NVSHMEM CUDA targets now omit the fused dispatch translation unit instead of compiling an empty macro-guarded source file.
    
    Reuse module-owned CUDA helpers from reconstruction and Riemann. The fused dispatch now calls reconstruction point helpers from src/recon/interp_impl.cuh and calls the existing lmars_impl/hllc_impl Riemann helpers instead of maintaining local copies of those formulas. The shared-memory stencil helpers in interp_impl.cuh remain intact for the regular reconstruction CUDA path.
    
    Validation: /home/chengcli/pyenv/bin/cmake -S . -B build (passed); /home/chengcli/pyenv/bin/cmake --build build --config Release --parallel 4 (passed; fused dispatch omitted from non-NVSHMEM target; existing libcudart.so.12 vs libcudart.so.13 linker warnings); /home/chengcli/pyenv/bin/cmake --build /tmp/snapy-nvshmem-cs --config Release --parallel 4 (passed; fused dispatch compiled with NVSHMEM; existing libcudart warning); ctest --output-on-failure -R '^(test_riemann.release|test_reconstruct.release|test_weno.release|test_cubed_sphere.release|test_exchange_ucx_cuda)$' (passed, 5/5); pre-commit run --files src/CMakeLists.txt src/recon/interp_impl.cuh src/hydro/fused_recon_riemann_dispatch.cu (active checks passed; clang-format hook skipped by matcher); git diff --check (passed).

--- src/hydro/fused_recon_riemann_dispatch.cu
+++ src/hydro/fused_recon_riemann_dispatch.cu
@@ -2,17 +2,17 @@
 #include <ATen/Dispatch.h>
 #include <ATen/cuda/CUDAContext.h>
 #include <c10/cuda/CUDAGuard.h>
-#ifndef NOT_USE_NVSHMEM
 #include <torch/csrc/distributed/c10d/symm_mem/CUDASymmetricMemory-inl.h>
-#endif
 
 // snap
 #include <snap/snap.h>
 
+#include "../recon/interp_impl.cuh"
+#include "../riemann/hllc_impl.h"
+#include "../riemann/lmars_impl.h"
 #include "fused_recon_riemann_dispatch.hpp"
 
 namespace snap {
-#ifndef NOT_USE_NVSHMEM
 namespace {
 
 template <typename T>
@@ -21,114 +21,18 @@ __device__ T sqr(T x) {
 }
 
 template <typename T>
-__device__ T coeff_poly3(bool right, int k) {
-  constexpr T cm[3] = {-1. / 3., 5. / 6., -1. / 6.};
-  return right ? cm[2 - k] : cm[k];
-}
-
-template <typename T>
-__device__ T coeff_poly5(bool right, int k) {
-  constexpr T cm[5] = {-1. / 20., 9. / 20., 47. / 60., -13. / 60., 1. / 30.};
-  return right ? cm[4 - k] : cm[k];
-}
-
-template <typename T>
-__device__ T interp_poly(T const* w, int v, int start, int stride_var,
-                         int stride_dim, FusedReconScheme scheme,
-                         bool right) {
-  T out = 0.;
-  if (scheme == FusedReconScheme::CP3) {
-    for (int k = 0; k < 3; ++k) {
-      out += coeff_poly3<T>(right, k) *
-             w[v * stride_var + (start + k) * stride_dim];
-    }
-  } else {
-    for (int k = 0; k < 5; ++k) {
-      out += coeff_poly5<T>(right, k) *
-             w[v * stride_var + (start + k) * stride_dim];
-    }
-  }
-  return out;
-}
-
-template <typename T>
-__device__ T vvdot(T const* x, T const* c, int n) {
-  T out = 0.;
-  for (int i = 0; i < n; ++i) out += x[i] * c[i];
-  return out;
-}
-
-template <typename T>
-__device__ T interp_weno3(T const* w, int v, int start, int stride_var,
-                          int stride_dim, bool right) {
-  constexpr T cm[4][3] = {{1. / 2., 1. / 2., 0.},
-                          {0., 3. / 2., -1. / 2.},
-                          {1., -1., 0.},
-                          {0., 1., -1.}};
-  T c[4][3];
-  for (int r = 0; r < 4; ++r) {
-    for (int k = 0; k < 3; ++k) c[r][k] = right ? cm[r][2 - k] : cm[r][k];
-  }
-  T phi[3];
-  for (int k = 0; k < 3; ++k) {
-    phi[k] = w[v * stride_var + (start + k) * stride_dim];
-  }
-  T p0 = vvdot(phi, c[0], 3);
-  T p1 = vvdot(phi, c[1], 3);
-  T beta0 = sqr(vvdot(phi, c[2], 3));
-  T beta1 = sqr(vvdot(phi, c[3], 3));
-  T alpha0 = (1.0 / 3.0) / sqr(beta0 + 1.e-6);
-  T alpha1 = (2.0 / 3.0) / sqr(beta1 + 1.e-6);
-  return (alpha0 * p0 + alpha1 * p1) / (alpha0 + alpha1);
-}
-
-template <typename T>
-__device__ T interp_weno5(T const* w, int v, int start, int stride_var,
-                          int stride_dim, bool right) {
-  constexpr T cm[9][5] = {
-      {-1. / 6., 5. / 6., 1. / 3., 0., 0.},
-      {0., 1. / 3., 5. / 6., -1. / 6., 0.},
-      {0., 0., 11. / 6., -7. / 6., 1. / 3.},
-      {1., -2., 1., 0., 0.},
-      {1., -4., 3., 0., 0.},
-      {0., 1., -2., 1., 0.},
-      {0., -1., 0., 1., 0.},
-      {0., 0., 1., -2., 1.},
-      {0., 0., 3., -4., 1.}};
-  T c[9][5];
-  for (int r = 0; r < 9; ++r) {
-    for (int k = 0; k < 5; ++k) c[r][k] = right ? cm[r][4 - k] : cm[r][k];
-  }
-  T phi[5];
-  for (int k = 0; k < 5; ++k) {
-    phi[k] = w[v * stride_var + (start + k) * stride_dim];
-  }
-  T p0 = vvdot(phi, c[0], 5);
-  T p1 = vvdot(phi, c[1], 5);
-  T p2 = vvdot(phi, c[2], 5);
-  T beta0 = 13. / 12. * sqr(vvdot(phi, c[3], 5)) +
-            .25 * sqr(vvdot(phi, c[4], 5));
-  T beta1 = 13. / 12. * sqr(vvdot(phi, c[5], 5)) +
-            .25 * sqr(vvdot(phi, c[6], 5));
-  T beta2 = 13. / 12. * sqr(vvdot(phi, c[7], 5)) +
-            .25 * sqr(vvdot(phi, c[8], 5));
-  T alpha0 = .3 / sqr(beta0 + 1.e-6);
-  T alpha1 = .6 / sqr(beta1 + 1.e-6);
-  T alpha2 = .1 / sqr(beta2 + 1.e-6);
-  return (alpha0 * p0 + alpha1 * p1 + alpha2 * p2) /
-         (alpha0 + alpha1 + alpha2);
-}
-
-template <typename T>
 __device__ T interp(T const* w, int v, int start, int stride_var,
                     int stride_dim, FusedReconScheme scheme, bool right) {
-  if (scheme == FusedReconScheme::CP3 || scheme == FusedReconScheme::CP5) {
-    return interp_poly(w, v, start, stride_var, stride_dim, scheme, right);
+  if (scheme == FusedReconScheme::CP3) {
+    return interp_point_poly3_impl(w, v, start, stride_var, stride_dim, right);
+  }
+  if (scheme == FusedReconScheme::CP5) {
+    return interp_point_poly5_impl(w, v, start, stride_var, stride_dim, right);
   }
   if (scheme == FusedReconScheme::WENO3) {
-    return interp_weno3(w, v, start, stride_var, stride_dim, right);
+    return interp_point_weno3_impl(w, v, start, stride_var, stride_dim, right);
   }
-  return interp_weno5(w, v, start, stride_var, stride_dim, right);
+  return interp_point_weno5_impl(w, v, start, stride_var, stride_dim, right);
 }
 
 template <typename T>
@@ -182,133 +86,6 @@ __device__ void eos_side_quantities(T const* wl, T const* wr, int ny,
   }
   *cl = sqrt((*gl) * wl[IPR] / wl[IDN]);
   *cr = sqrt((*gr) * wr[IPR] / wr[IDN]);
-}
-
-template <typename T>
-__device__ void fused_lmars(T* flux, T const* wl, T const* wr, T hl, T hr,
-                            T gammal, T gammar, int dim, int ny,
-                            int stride_var) {
-  auto ivx = IPR - dim;
-  auto ivy = IVX + ((ivx - IVX) + 1) % 3;
-  auto ivz = IVX + ((ivx - IVX) + 2) % 3;
-
-  T wli[5] = {wl[IDN], wl[ivx], wl[ivy], wl[ivz], wl[IPR]};
-  T wri[5] = {wr[IDN], wr[ivx], wr[ivy], wr[ivz], wr[IPR]};
-
-  hl += 0.5 * (sqr(wli[IVX]) + sqr(wli[IVY]) + sqr(wli[IVZ])) +
-        wli[IPR] / wli[IDN];
-  hr += 0.5 * (sqr(wri[IVX]) + sqr(wri[IVY]) + sqr(wri[IVZ])) +
-        wri[IPR] / wri[IDN];
-
-  auto rhobar = 0.5 * (wli[IDN] + wri[IDN]);
-  auto gamma_bar = 0.5 * (gammal + gammar);
-  auto cbar = sqrt(0.5 * gamma_bar * (wli[IPR] + wri[IPR]) / rhobar);
-
-  auto pbar = 0.5 * (wli[IPR] + wri[IPR]) +
-              0.5 * (rhobar * cbar) * (wli[IVX] - wri[IVX]);
-
-  auto ubar = 0.5 * (wli[IVX] + wri[IVX]) +
-              0.5 / (rhobar * cbar) * (wli[IPR] - wri[IPR]);
-
-  auto side = ubar > 0.0 ? wl : wr;
-  auto side5 = ubar > 0.0 ? wli : wri;
-  auto h = ubar > 0.0 ? hl : hr;
-  T rd = 1.0;
-  for (int n = 0; n < ny; n++) rd -= side[ICY + n];
-
-  flux[IDN * stride_var] = ubar * side5[IDN] * rd;
-  for (int n = 0; n < ny; n++) {
-    flux[(ICY + n) * stride_var] = ubar * side5[IDN] * side[ICY + n];
-  }
-  flux[ivx * stride_var] = ubar * side5[IDN] * side5[IVX] + pbar;
-  flux[ivy * stride_var] = ubar * side5[IDN] * side5[IVY];
-  flux[ivz * stride_var] = ubar * side5[IDN] * side5[IVZ];
-  flux[IPR * stride_var] = ubar * side5[IDN] * h;
-}
-
-template <typename T>
-__device__ void fused_hllc(T* flux, T const* wl, T const* wr, T el, T er,
-                           T gammal, T gammar, T cl, T cr, int dim, int ny,
-                           int stride_var) {
-  auto tiny = T(1.e-10);
-  auto ivx = IPR - dim;
-  auto ivy = IVX + ((ivx - IVX) + 1) % 3;
-  auto ivz = IVX + ((ivx - IVX) + 2) % 3;
-
-  el += 0.5 * wl[IDN] * (sqr(wl[IVX]) + sqr(wl[IVY]) + sqr(wl[IVZ]));
-  er += 0.5 * wr[IDN] * (sqr(wr[IVX]) + sqr(wr[IVY]) + sqr(wr[IVZ]));
-
-  auto rhoa = .5 * (wl[IDN] + wr[IDN]);
-  auto ca = .5 * (cl + cr);
-  auto pmid = .5 * (wl[IPR] + wr[IPR] + (wl[ivx] - wr[ivx]) * rhoa * ca);
-
-  auto ql = (pmid <= wl[IPR])
-                ? T(1.0)
-                : sqrt(1.0 + (gammal + 1) / (2 * gammal) *
-                                  (pmid / wl[IPR] - 1.0));
-  auto qr = (pmid <= wr[IPR])
-                ? T(1.0)
-                : sqrt(1.0 + (gammar + 1) / (2 * gammar) *
-                                  (pmid / wr[IPR] - 1.0));
-
-  auto al = wl[ivx] - cl * ql;
-  auto ar = wr[ivx] + cr * qr;
-  auto bp = ar > 0.0 ? ar : tiny;
-  auto bm = al < 0.0 ? al : -tiny;
-
-  auto vxl = wl[ivx] - al;
-  auto vxr = wr[ivx] - ar;
-  auto tl = wl[IPR] + vxl * wl[IDN] * wl[ivx];
-  auto tr = wr[IPR] + vxr * wr[IDN] * wr[ivx];
-  auto ml = wl[IDN] * vxl;
-  auto mr = -(wr[IDN] * vxr);
-
-  auto am = (tl - tr) / (ml + mr);
-  auto cp = (ml * tr + mr * tl) / (ml + mr);
-  cp = cp > 0.0 ? cp : 0.0;
-
-  vxl = wl[ivx] - bm;
-  vxr = wr[ivx] - bp;
-
-  T rdl = 1., rdr = 1.;
-  for (int n = 0; n < ny; ++n) {
-    rdl -= wl[ICY + n];
-    rdr -= wr[ICY + n];
-  }
-
-  T fl[5], fr[5];
-  fl[IDN] = wl[IDN] * vxl * rdl;
-  fr[IDN] = wr[IDN] * vxr * rdr;
-  fl[ivx] = wl[IDN] * wl[ivx] * vxl + wl[IPR];
-  fr[ivx] = wr[IDN] * wr[ivx] * vxr + wr[IPR];
-  fl[ivy] = wl[IDN] * wl[ivy] * vxl;
-  fr[ivy] = wr[IDN] * wr[ivy] * vxr;
-  fl[ivz] = wl[IDN] * wl[ivz] * vxl;
-  fr[ivz] = wr[IDN] * wr[ivz] * vxr;
-  fl[IPR] = el * vxl + wl[IPR] * wl[ivx];
-  fr[IPR] = er * vxr + wr[IPR] * wr[ivx];
-
-  T sl, sr, sm;
-  if (am >= 0.0) {
-    sl = am / (am - bm);
-    sr = 0.0;
-    sm = -bm / (am - bm);
-  } else {
-    sl = 0.0;
-    sr = -am / (bp - am);
-    sm = bp / (bp - am);
-  }
-
-  flux[IDN * stride_var] = sl * fl[IDN] + sr * fr[IDN];
-  flux[ivx * stride_var] = sl * fl[ivx] + sr * fr[ivx] + sm * cp;
-  flux[ivy * stride_var] = sl * fl[ivy] + sr * fr[ivy];
-  flux[ivz * stride_var] = sl * fl[ivz] + sr * fr[ivz];
-  flux[IPR * stride_var] = sl * fl[IPR] + sr * fr[IPR] + sm * cp * am;
-  for (int n = 0; n < ny; ++n) {
-    auto fln = wl[IDN] * wl[ICY + n] * vxl;
-    auto frn = wr[IDN] * wr[ICY + n] * vxr;
-    flux[(ICY + n) * stride_var] = sl * fln + sr * frn;
-  }
 }
 
 template <typename T>
@@ -371,11 +148,11 @@ __global__ void fused_kernel(T const* w, T* flux, int nvar, int nc3, int nc2,
                       cv_ratio_m1, u0, &el, &er, &gl, &gr, &cl, &cr);
 
   if (solver == FusedRiemannSolver::LMARS) {
-    fused_lmars(flux + flat, wl_local, wr_local, el / wl_local[IDN],
-                er / wr_local[IDN], gl, gr, dim, ny, stride_var);
+    lmars_impl(flux + flat, wl_local, wr_local, el / wl_local[IDN],
+               er / wr_local[IDN], gl, gr, dim, ny, stride_var);
   } else {
-    fused_hllc(flux + flat, wl_local, wr_local, el, er, gl, gr, cl, cr, dim,
-               ny, stride_var);
+    hllc_impl(flux + flat, wl_local, wr_local, el, er, gl, gr, cl, cr, dim, ny,
+              stride_var);
   }
 }
 
@@ -674,10 +451,10 @@ __global__ void cs_flux_kernel(T const* w, T* flux2, T* flux3, void** buf_ptrs,
   int flux_flat = flux_k * nc2 * nc1 + flux_j * nc1 + i;
   T* flux = side <= CS_SIDE_R ? flux2 + flux_flat : flux3 + flux_flat;
   if (solver == FusedRiemannSolver::LMARS) {
-    fused_lmars(flux, wl, wr, el / wl[IDN], er / wr[IDN], gl, gr, dim, ny,
-                stride_var);
+    lmars_impl(flux, wl, wr, el / wl[IDN], er / wr[IDN], gl, gr, dim, ny,
+               stride_var);
   } else {
-    fused_hllc(flux, wl, wr, el, er, gl, gr, cl, cr, dim, ny, stride_var);
+    hllc_impl(flux, wl, wr, el, er, gl, gr, cl, cr, dim, ny, stride_var);
   }
 }
 
@@ -765,6 +542,4 @@ void fused_cubed_sphere_exchange_cuda(
   });
   C10_CUDA_KERNEL_LAUNCH_CHECK();
 }
-#endif
-
 }  // namespace snap
